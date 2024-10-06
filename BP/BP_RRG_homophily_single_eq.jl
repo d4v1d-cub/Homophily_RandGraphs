9712852600c6fbaa6ef25d120e6c4d9e4423450d
David Machado <davidmachadop@gmail.com>
2024-10-06 14:57:40 +0200

    compute free energy

--- BP/BP_RRG_homophily_single_eq.jl
+++ BP/BP_RRG_homophily_single_eq.jl
@@ -28,9 +28,9 @@ function set_messages_rand(seed::Int64, G::Int64)
 end
 
 
-function set_messages_ord(G::Int64)
+function set_messages_ord(conf::Int64, G::Int64)
     messages=zeros(2^G)
-    messages[1] = 1
+    messages[conf] = 1
     messages_new=deepcopy(messages)
     return messages, messages_new
 end
@@ -95,7 +95,7 @@ function ener(G::Int64, spins_combs::Array{Array{Int8, 1}, 1},
             cumul_prob += messages[comb_i] * messages[comb_j] * exp_hamilt
         end
     end
-    return cumul_edge / cumul_prob
+    return cumul_edge / cumul_prob, cumul_prob
 end
 
 
@@ -139,7 +139,7 @@ function mag_norm(G::Int64, spins_combs::Array{Array{Int8, 1}, 1},
     av_vector = cumul_num / cumul_den
 
     norm = sqrt(sum(av_vector .^ 2) / G)
-    return norm, av_vector
+    return norm, av_vector, cumul_den
 end
 
 
@@ -199,24 +199,25 @@ function run_BP_find_transition(c, seed, string1, string_init, max_iter, delta, 
                           string(max_iter), string_init, "_G_", string(G), ".txt")
     w = open(fileeq, "w")
     f_comp = open(filemag_comp, "w") 
-    write(w, "#  T   alpha   e   m(norm)    m(1)   convergence\n")
+    write(w, "#  T   alpha   e   m(norm)    m(1)   free_ener   convergence\n")
     write(f_comp, "#  T   alpha   mag_components\n")
     for alpha in alphas_list
         if cond_init == "rand"
             messages, messages_new = set_messages_rand(seed, G)
         elseif cond_init == "ord"
-            messages, messages_new = set_messages_ord(G)
+            messages, messages_new = set_messages_ord(seed, G)
         end
         conv = belief_propagation(temp, c, max_iter, delta, G, alpha, spins_combs, 
                                   messages, messages_new)
         if !conv
             println("BP did not converge for T=", temp, " and alpha=",alpha)
         end
-        eq_e = ener(G, spins_combs, messages, alpha, temp, c)
-        eq_m_norm, mag_components = mag_norm(G,spins_combs,messages,c, alpha, temp)
+        eq_e, zij = ener(G, spins_combs, messages, alpha, temp, c)
+        eq_m_norm, mag_components, zi = mag_norm(G,spins_combs,messages,c, alpha, temp)
         eq_m_oriented = mag_towards_one(c, G, spins_combs, messages, alpha, temp)
+        f = -temp * (log(zi) - c * log(zij) / 2) 
         write(w, string(temp) * "\t" * string(alpha) * "\t" * string(eq_e) * "\t" *string(eq_m_norm) 
-                 * "\t" *string(eq_m_oriented) * "\t" * string(conv) * "\n")
+                 * "\t" *string(eq_m_oriented) * "\t" * string(f) * "\t" * string(conv) * "\n")
         write(f_comp, string(temp) * "\t" * string(alpha))
         for comp in mag_components
             write(f_comp, "\t" * string(comp))
@@ -253,7 +254,6 @@ c = parse(Int64, ARGS[1])      # Graph connectivity
 exponent = parse(Int64, ARGS[2])
 delta = 10.0 ^ exponent   # When the error is below this threshold, I declare that BP converges
 max_iter = 100000            # BP will stop after "max_iter" iterations
-seed = 1                  # The seed for the initial messages
 string1 = "RGfc"          # An identification of the graph you are using. This string is insterted in 
                           # the output strings
 G = parse(Int64, ARGS[3])
@@ -266,13 +266,15 @@ temp_range=range(0.01,stop=0.54,step=0.01)
 temp_list = collect(temp_range)
 
 cond_init = ARGS[4]
+seed = parse(Int64, ARGS[5])                  # The seed for the initial messages in case 
+# cond_init is 'rand', or the specific initial configuration in case cond_init=ord
 
 println("cond_init=" * cond_init)
 
 if cond_init == "rand"
     string_init = string("_rand_init_seed_", string(seed))
 elseif cond_init == "ord"
-    string_init = "_init_ord"
+    string_init = string("_ord_init_conf_", string(seed))
 else
     println("The initial condition variable 'cond_init' must be 'ord' or 'rand'")
     exit(1)
